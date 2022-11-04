653965ba6b00d56b33aed3aa67869ab3b0e6430f
ABHISHEK KUMAR <101967057+abhishekadityaroy102@users.noreply.github.com>
2022-11-04 11:49:55 +0530

    complete

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { connection } = require("./db");
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Welcome to my world!");
+});
+app.listen(port, async () => {
+  try {
+    await connection();
+    console.log("connected");
+  } catch {
+    console.log(err.message);
+  }
+  console.log("listening on port no : 8080");
+});
