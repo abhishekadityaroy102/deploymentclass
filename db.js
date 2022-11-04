05e67119e9a0ae0af353ae388e6287032a23fc67
ABHISHEK KUMAR <101967057+abhishekadityaroy102@users.noreply.github.com>
2022-11-04 12:21:40 +0530

    change db dlete

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
-const mongoose = require("mongoose");
-const connection = () =>
-  mongoose.connect(
-    "mongodb+srv://demotest:Demotest@cluster0.dr3kwiq.mongodb.net/demodata?retryWrites=true&w=majority"
-  );
-module.exports = { connection };
+// const mongoose = require("mongoose");
+// const connection = () =>
+//   mongoose.connect(
+//     "mongodb+srv://demotest:Demotest@cluster0.dr3kwiq.mongodb.net/demodata?retryWrites=true&w=majority"
+//   );
+// module.exports = { connection };
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-require("dotenv").config;
+require("dotenv").config();
 const { connection } = require("./db");
 const port = process.env.PORT || 8080;
 const app = express();
@@ -8,11 +8,11 @@ app.get("/", (req, res) => {
   res.send("Welcome to my world!");
 });
 app.listen(port, async () => {
-  try {
-    await connection();
-    console.log("connected");
-  } catch {
-    console.log(err.message);
-  }
+  // try {
+  //   await connection();
+  //   console.log("connected");
+  // } catch {
+  //   console.log(err.message);
+  // }
   console.log("listening on port no : 8080");
 });
